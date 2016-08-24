cf3a8252c332ae7e340e175b25d58764ff896137
Sandeep Thandassery <saraveen@visa.com>
2016-08-23 22:02:13 -0500

    project prints datetime to response

---
+++ servletpro/src/main/java/servletpro/DateTimeServlet.java
@@ -0,0 +1,22 @@
+package servletpro;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/datetime" })
+public class DateTimeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 616518295634927631L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.getWriter().write(LocalDateTime.now().toString());
+	}
+}
