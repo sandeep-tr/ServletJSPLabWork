dac05c10e9c85c5983aa62152717e8122408899e
Sandeep Thandassery <sandeep.thandassery@yahoo.com>
2016-08-29 23:36:45 -0500

    Day6: Object persistence using Hibernate added

--- hibernate-pro/src/main/java/com/bootcamp/main/Main.java
+++ hibernate-pro/src/main/java/com/bootcamp/main/Main.java
@@ -1,17 +1,37 @@
 package com.bootcamp.main;
 
 import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
+import org.hibernate.Session;
 
+import com.bootcamp.objects.User;
 import com.bootcamp.utilities.HibernateUtilities;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Checking Hibernate Session Factory");
 		try {
-			SessionFactory session = HibernateUtilities.getSessionFactory();
-			session.openSession();
+			Session session = HibernateUtilities.getSessionFactory().openSession();
+			// -- INSERT
+			session.beginTransaction();
+
+			User user = new User();
+			user.setName("Sandeep");
+			user.setGoal(250);
+			session.save(user);
+
+			session.getTransaction().commit();
+
+			// -- SELECT
+			session.beginTransaction();
+
+			User savedUser = (User) session.get(User.class, 1);
+			savedUser.setTotal(savedUser.getTotal() + 50);
+			System.out.println(savedUser);
+
+			session.getTransaction().commit();
+
 			session.close();
+			HibernateUtilities.getSessionFactory().close();
 		} catch (HibernateException hbe) {
 			System.out.println(hbe);
 		}
